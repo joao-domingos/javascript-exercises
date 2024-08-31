86f60770a7468bd7570c645f1f9f86f2b117856c
joao-domingos <joaopedro.mdomingos@gmail.com>
2024-08-30 23:22:48 -0300

    ex 1-3 resolvidos

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, quantity) {
+    let palavra = '';
+    if (quantity < 0) {
+        palavra = 'ERROR'
+        return palavra;
+    }
+    else {
+        for (let i = 0; i < quantity; i++) {
+            palavra += word;
+        }
+        return  palavra;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let array = word.split('').reverse().join('');
+    return array;
 };
 
 // Do not edit below this line
