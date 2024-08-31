15d0fca493e03e1e0d6d5b465372c9bc7d9cf43f
joao-domingos <joaopedro.mdomingos@gmail.com>
2024-08-31 20:44:19 -0300

    ex 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,49 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    /*
+    2/8
+
+    //indexOf localiza o index especificado
+    const index = array.indexOf(toRemove);
+    //esse if verifica se o index existe
+    if (index > -1) {
+        //splice remove um argumento especificado quantas vezes forem mandadas
+        array.splice(index, 5);
+    }
+    return array;
+    */
+
+    /*
+    3/8 
+    const elementsToRemove = array.filter(element => element === toRemove);
+    const newArray = array.filter(element => !elementsToRemove.includes(element));
+
+    return newArray;
+    */
+
+    /*
+    1/8
+
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === args) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+    */
+
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, fim) {
+    let summ = 0;
 
+    if (
+        typeof start !== 'number' || 
+        typeof fim !== 'number' ||
+        start < 0 ||
+        fim < 0 ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(fim)
+    ) {
+        return 'ERROR';
+    }
+    if (start > fim) {
+        let aux;
+        aux = fim;
+        fim = start;
+        start = aux;
+    }
+    while (start < fim + 1) {
+        summ += start;
+        start++;
+    }
+
+    return summ;
 };
 
 // Do not edit below this line
